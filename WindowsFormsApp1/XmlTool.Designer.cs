16a30796b604bf2ced5e806c03a1dcc1678f7aaf
wangzhe66369 <wangzhe66369@Gmail.com>
2019-09-06 15:52:04 +0800

    Replace the value of the xml value tag with the target xml

---
+++ WindowsFormsApp1/XmlTool.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class XmlTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtSoundPath2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtSoundPath1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.prompt = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.bw = new System.ComponentModel.BackgroundWorker();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.702413F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 93.29758F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 91F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableLayoutPanel1.Controls.Add(this.txtSoundPath2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtSoundPath1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.progressBar1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.prompt, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnStop, 4, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(60, 62);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 69F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(703, 243);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtSoundPath2
+            // 
+            this.txtSoundPath2.Location = new System.Drawing.Point(34, 66);
+            this.txtSoundPath2.Name = "txtSoundPath2";
+            this.txtSoundPath2.Size = new System.Drawing.Size(428, 20);
+            this.txtSoundPath2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(509, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Source";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(509, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "GO";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtSoundPath1
+            // 
+            this.txtSoundPath1.Location = new System.Drawing.Point(34, 3);
+            this.txtSoundPath1.Name = "txtSoundPath1";
+            this.txtSoundPath1.Size = new System.Drawing.Size(428, 20);
+            this.txtSoundPath1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Target";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(600, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "将要修改的xml";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(600, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "提供修改的数据";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(34, 129);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(400, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // prompt
+            // 
+            this.prompt.Location = new System.Drawing.Point(34, 177);
+            this.prompt.Name = "prompt";
+            this.prompt.ReadOnly = true;
+            this.prompt.Size = new System.Drawing.Size(145, 20);
+            this.prompt.TabIndex = 5;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(600, 129);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // bw
+            // 
+            this.bw.WorkerReportsProgress = true;
+            this.bw.WorkerSupportsCancellation = true;
+            // 
+            // XmlTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "XmlTool";
+            this.Text = "XmlTool";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSoundPath1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSoundPath2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox prompt;
+        private System.ComponentModel.BackgroundWorker bw;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ WindowsFormsApp1/XmlTool.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class XmlTool : Form
+    {
+        private BackgroundWorker bgWorker = new BackgroundWorker();
+        XDocument docTarget = null;
+        IEnumerable<XElement> target = null;
+        IEnumerable<XElement> elementsSource = null;
+ 
+        public XmlTool()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+        string filePath1 = string.Empty;
+        string filePath2 = string.Empty;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //创建文件弹出选择窗口（包括文件名）对象
+            OpenFileDialog ofd = new OpenFileDialog();
+            //判断选择的路径
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtSoundPath1.Text = ofd.FileName.ToString();
+            }
+            filePath1 = this.txtSoundPath1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //创建文件弹出选择窗口（包括文件名）对象
+            OpenFileDialog ofd = new OpenFileDialog();
+            //判断选择的路径
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtSoundPath2.Text = ofd.FileName.ToString();
+            }
+            filePath2 = this.txtSoundPath2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (bgWorker.IsBusy)
+                return;
+
+            //将要修改的xml
+            docTarget = XDocument.Load(filePath1);
+            XElement rootNodeTarget = docTarget.Root;
+            var childTarget = docTarget.Descendants().Where(d => d.Name == "data");
+            target = childTarget.Elements();
+            this.progressBar1.Maximum = target.Count();
+            //提供修改的数据
+            var docSource = XDocument.Load(filePath2);
+            XElement rootNodeSource = docSource.Root;
+            elementsSource = rootNodeSource?.Elements();
+            
+            //this.progressBar1.Maximum = 100;
+            bgWorker.RunWorkerAsync("hello");
+        }
+
+
+        private void InitializeBackgroundWorker()
+        {
+            bgWorker.WorkerReportsProgress = true;//能否报告进度更新。
+            bgWorker.WorkerSupportsCancellation = true;//是否支持异步取消
+            bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+            bgWorker.ProgressChanged += new ProgressChangedEventHandler(bgWorker_ProgessChanged);
+            bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_WorkerCompleted);
+        }
+
+        public void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //句柄sender指向的就是该BackgroundWorker。
+
+            //e.Argument 获取异步操作参数的值  
+            //e.Cancel 是否应该取消事件
+            //e.Result  获取或设置异步操作结果的值(在RunWorkerCompleted事件可能会使用到)
+            //object a = e.Argument;//获取RunWorkerAsync(object argument)传入的值
+
+        
+            int i = 0;
+            int j = 0;
+            foreach (var item in target)
+            {
+                if (bgWorker.CancellationPending)//在耗时操作中判断CancellationPending属性，如果为false则退出
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                else
+                {
+                   
+                    string parentvalue = item.Parent.Attribute("name").Value;
+                    var source = elementsSource.Where(d => d.Attribute("name")?.Value == parentvalue);
+                    var sourceValue = source?.Descendants()?.FirstOrDefault()?.Value;
+                    if (sourceValue != null)
+                    {
+                        if (item.Value != sourceValue)
+                        {
+                            if (item.Name == "value")
+                            {
+                                j++;
+                                item.Value = sourceValue;
+                            }
+                            else if (item.Name == "comment")
+                            {
+                                item.Remove();
+                            }
+                            docTarget.Save(filePath1);
+                        }
+                    }
+                    
+                    i++;
+                    bgWorker.ReportProgress(i, j);// 将触发BackgroundWorker.ProgressChanged事件，向ProgressChanged报告进度
+                }
+            }
+        }
+
+        public void bgWorker_ProgessChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //string state = (string)e.UserState;//接收ReportProgress方法传递过来的userState
+            this.progressBar1.Value = e.ProgressPercentage;
+            //e.ProgressPercentage  获取异步操作进度的百分比
+            //this.prompt.Text = "处理条数:" + Convert.ToString(e.ProgressPercentage);
+            this.prompt.Text = "处理条数:" + Convert.ToString(e.UserState);
+
+        }
+        /// <summary>
+        /// 当DoWork事件处理完成之后，将会触发该事件。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bgWorker_WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            //e.Cancelled指示异步操作是否已被取消
+            //e.Error 指示异步操作期间发生的错误
+            //e.Result 获取异步操作结果的值,即DoWork事件中，Result设置的值。
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.ToString());
+                return;
+            }
+            //if (!e.Cancelled)
+            //    this.prompt.Text = "处理完毕!";
+            //else
+            //    this.prompt.Text = "处理终止!";
+
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            bgWorker.CancelAsync();
+        }
+    }
+}
